363224ac42ca75416d6b7e855e4a532ac19f8517
Michal Kučera <mikucera@centrum.cz>
2023-01-08 08:55:33 +0100

    "Optimalizace kódu hry."

--- Game.py
+++ Game.py
@@ -1,5 +1,3 @@
-import random
-
 # Hra:
 # Predstavte si, ze jsem vas zakaznik a chci od Vas vytvorit v Pythonu hru simulaci areny s
 # bojovniky. Kreativite se meze nekladou, ale zakladni koncept je, ze mezi sebou budou
@@ -23,69 +21,106 @@ import random
 # programatori! Az se naucite pracovat s Gitem, urcite si hru ulozte na svuj Git jako
 # ukazku Vasich schopnosti a sdilejte ji se skupinou i se mnou! :)
 # Dale zapojte kreativitu a vylepsite si hru podle libosti, tesim se na Vase vysledky! :)
+import random
+
+
+class Zbran:
+    def __init__(self, typ, sila):
+        self.typ = typ
+        self.sila = sila
+
 
 class Bojovnik:
-
-    def __init__(self, jmeno, zbran, pocet_zivotu, stit: bool, bojove_zkusenosti):
+    def __init__(self, jmeno, zdravi, sila, zbran, ma_stit, bonusove_poskozeni, pocet_let_bojovych_zkusenosti,
+                 pocet_zivotu):
         self.jmeno = jmeno
+        self.zdravi = zdravi
+        self.sila = sila
         self.zbran = zbran
+        self.ma_stit = ma_stit
+        self.bonusove_poskozeni = bonusove_poskozeni
+        self.pocet_let_bojovych_zkusenosti = pocet_let_bojovych_zkusenosti
         self.pocet_zivotu = pocet_zivotu
-        self.stit = stit
-        self.bojove_zkusenosti = bojove_zkusenosti
-        self.sila_zbrane = self.ucinnost_zbrane()
 
-        self.zbrane = {"mec": 75, "dyka": 50, "kopi": 80, "palcat": 45, "maceta": 70}
-
-    def ucinnost_zbrane(self):
-        if self.zbran == "mec":
-            ef_zbr = random.randrange(50, 76)
-        elif self.zbran == "dyka":
-            ef_zbr = random.randrange(15, 36)
-        elif self.zbran == "kopi":
-            ef_zbr = random.randrange(40, 96)
-        elif self.zbran == "palcat":
-            ef_zbr = random.randrange(15, 45)
-        elif self.zbran == "maceta":
-            ef_zbr = random.randrange(40, 76)
-        else:
-            ef_zbr = random.randrange(1, 11)
-
-        return ef_zbr
-
-    def jmeno(self):
-        return self.jmeno
-
-    def utok(self):
-        return (random.randint(0, 100) + self.zbrane.get(self.zbran) + self.bojove_zkusenosti) / 3
-
-    def obrana(self):
-        return (random.randint(0, 100) + self.bojove_zkusenosti + (100 * self.stit)) / 3 * self.pocet_zivotu / 100
-
-    def uder(self, protivnik):
-        protivnik.pocet_zivotu -= self.sila_zbrane*random.random()
-        if protivnik.pocet_zivotu <= 0:
-            protivnik.pocet_zivotu = 0
+    def __str__(self):
+        return f"Jmeno: {self.jmeno}, Zdravi: {self.zdravi}, Sila: {self.sila}, " \
+               f"Zbran: {self.zbran}, Ma stit: {self.ma_stit}, Bonusove poskozeni: {self.bonusove_poskozeni}, " \
+               f"Pocet let bojovych zkusenosti: {self.pocet_let_bojovych_zkusenosti}, " \
+               f"Pocet zivotu: {self.pocet_zivotu}"
 
 
-Robin_bojovnik = Bojovnik("Robin", "mec", 100, True, 80)
-Jan_bojovnik = Bojovnik("Jan", "mec", 100, False, 80)
+class Sermir(Bojovnik):
+    def __init__(self, jmeno, zdravi, sila, zbran, ma_stit, bonusove_poskozeni, pocet_let_bojovych_zkusenosti,
+                 pocet_zivotu):
+        super().__init__(jmeno, zdravi, sila, zbran, ma_stit, bonusove_poskozeni, pocet_let_bojovych_zkusenosti,
+                         pocet_zivotu)
 
-while Jan_bojovnik.pocet_zivotu > 0 or Robin_bojovnik.pocet_zivotu > 0:
 
-    if Robin_bojovnik.pocet_zivotu != 0:
-        Robin_bojovnik.uder(Jan_bojovnik)
-        print(f"životy Jana {Jan_bojovnik.pocet_zivotu: .0f}")
-        if Jan_bojovnik.pocet_zivotu != 0:
-            Jan_bojovnik.uder(Robin_bojovnik)
-            print(f"životy Robina {Robin_bojovnik.pocet_zivotu: .0f}")
-        else:
-            break
+class Lukostrelec(Bojovnik):
+    def __init__(self, jmeno, zdravi, sila, zbran, ma_stit, bonusove_poskozeni, pocet_let_bojovych_zkusenosti,
+                 pocet_zivotu):
+        super().__init__(jmeno, zdravi, sila, zbran, ma_stit, bonusove_poskozeni, pocet_let_bojovych_zkusenosti,
+                         pocet_zivotu)
+
+
+class Mag(Bojovnik):
+    def __init__(self, jmeno, zdravi, sila, zbran, ma_stit, bonusove_poskozeni, pocet_let_bojovych_zkusenosti,
+                 pocet_zivotu):
+        super().__init__(jmeno, zdravi, sila, zbran, ma_stit, bonusove_poskozeni, pocet_let_bojovych_zkusenosti,
+                         pocet_zivotu)
+
+
+def stret_bojovniku(utocnik: Bojovnik, obrance: Bojovnik):
+    # TODO tady si muzes pohrat se stretem, prvky randomizace apod.
+    print("utocnik", utocnik.jmeno, utocnik.zbran.sila * utocnik.sila,
+          "obrance", obrance.jmeno, int(obrance.ma_stit) * 100 * obrance.sila)
+
+    if (utocnik.zbran.sila * utocnik.sila) > (int(obrance.ma_stit) * obrance.sila):
+        obrance.pocet_zivotu -= 1
+        print(f"vyhra utocnika")
     else:
-        break
+        print(f"vyhra obrance")
 
-    if Robin_bojovnik.pocet_zivotu > Jan_bojovnik.pocet_zivotu:
-        print("Vítězem souboje je Robin.")
-    elif Robin_bojovnik.pocet_zivotu < Jan_bojovnik.pocet_zivotu:
-        print("Vítězem souboje je Jan.")
-    else:
-        print("Oba bojovníci utrpěli stejné poškození. Boj skončil remízou.")
+
+class Arena:
+    def __init__(self, bojovnik1: Bojovnik, bojovnik2: Bojovnik):
+        self.bojovnik1 = bojovnik1
+        self.bojovnik2 = bojovnik2
+
+    def rozhodni_utocnik_obrance(self):
+        if random.choice([self.bojovnik1, self.bojovnik2]) == self.bojovnik1:
+            return self.bojovnik1, self.bojovnik2
+        else:
+            return self.bojovnik2, self.bojovnik1
+
+    def boj(self):
+        while self.bojovnik1.pocet_zivotu > 0 and self.bojovnik2.pocet_zivotu > 0:
+            utocici_bojovnik, branici_bojovnik = self.rozhodni_utocnik_obrance()
+            # print(f"utoci bojovnik: {utocici_bojovnik} , branici bojovnik: {branici_bojovnik}")
+            stret_bojovniku(utocici_bojovnik, branici_bojovnik)
+
+        if self.bojovnik1.pocet_zivotu > self.bojovnik2.pocet_zivotu:
+            print(f"Vítězem souboje je {self.bojovnik1.jmeno}.")
+        elif self.bojovnik1.pocet_zivotu < self.bojovnik2.pocet_zivotu:
+            print(f"Vítězem souboje je {self.bojovnik2.jmeno}.")
+        else:
+            print("Oba bojovníci utrpěli stejné poškození. Boj skončil remízou.")
+
+
+z1 = Zbran(typ="mec", sila=50)
+z2 = Zbran(typ="luk", sila=60)
+
+b1 = Sermir(
+    jmeno="Pan Sermir", zdravi=100, sila=40, zbran=z1, ma_stit=False,
+    bonusove_poskozeni=20, pocet_let_bojovych_zkusenosti=2, pocet_zivotu=4
+)
+b2 = Lukostrelec(
+    jmeno="Pan Lukous", zdravi=100, sila=20, zbran=z2, ma_stit=True,
+    bonusove_poskozeni=50, pocet_let_bojovych_zkusenosti=3, pocet_zivotu=3
+)
+
+print(b1)  # vysledek metody __str__()
+print(b2)  # vysledek metody __str__()
+
+hra = Arena(bojovnik1=b1, bojovnik2=b2)
+hra.boj()
